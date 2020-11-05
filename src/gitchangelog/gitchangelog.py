fa496a29ac95e98a564c4fe38ca50e52f0de7383
Stephen L Arnold <nerdboy@gentoo.org>
2020-11-04 17:22:00 -0800

    move version var to module level and read it via attr in setup.cfg
    Signed-off-by: Stephen L Arnold <nerdboy@gentoo.org>

--- src/gitchangelog/gitchangelog.py
+++ src/gitchangelog/gitchangelog.py
@@ -19,6 +19,8 @@ import errno
 
 from subprocess import Popen, PIPE
 
+from . import __version__
+
 try:
     import pystache
 except ImportError:  ## pragma: no cover
@@ -29,8 +31,6 @@ try:
 except ImportError:  ## pragma: no cover
     mako = None
 
-
-__version__ = '3.0.4'
 
 DEBUG = None
 
